c130c005218c9bdd81e95384f77ca9f56d93cf68
Christian <c.nisen@gmail.com>
2019-02-23 01:43:40 -0600

    test to ensure that the new text is displayed when the button is clicked added

--- tests/test_front_end.py
+++ tests/test_front_end.py
@@ -28,6 +28,11 @@ class TestBase(LiveServerTestCase):
         response = urlopen(self.get_server_url())
         self.assertEqual(response.code, 200)
 
+# test to ensure that the new text is displayed when the button is clicked.
+    def test_text_displayed_button(self):
+        self.driver.find_element_by_id("button").click()
+        self.assertEqual("Hello World!! Welcome!", driver.find_element_by_id("text").text)
+
 
 if __name__ == '__main__':
     unittest.main()
